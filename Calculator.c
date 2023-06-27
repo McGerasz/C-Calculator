03b55e951de13795d86340b5f9fc8faf3fcf1d0e
McGerasz <mcgerasz@gmail.com>
2023-06-27 14:38:47 +0200

    refactored float inputs

--- Calculator.c
+++ Calculator.c
@@ -1,46 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-
-void sum(){
-    float firstNum;
+void floatInput(float *firstNum, float *secondNum){
     printf("Enter the first number: ");
-    scanf("%f", &firstNum);
-    float secondNum;
+    scanf("%f", firstNum);
     printf("\nEnter the second number: ");
-    scanf("%f", &secondNum);
+    scanf("%f", secondNum);
+}
+void sum(){
+    float firstNum, secondNum;
+    floatInput(&firstNum, &secondNum);
     float answer = firstNum + secondNum;
     if((int)answer == answer) printf("The sum of the provided numbers is: %d", (int)answer);
     else printf("The sum of the provided numbers is: %.2f", firstNum + secondNum);
 }
 void subt(){
-    float firstNum;
-    printf("Enter the first number: ");
-    scanf("%f", &firstNum);
-    float secondNum;
-    printf("\nEnter the second number: ");
-    scanf("%f", &secondNum);
+    float firstNum, secondNum;
+    floatInput(&firstNum, &secondNum);
     float answer = firstNum  - secondNum;
     if((int)answer == answer) printf("The second number subtracted from the first number is: %d", (int)answer);
     else printf("The second number subtracted from the first number is: %.2f", firstNum - secondNum);
 }
 void mult(){
-    float firstNum;
-    printf("Enter the first number: ");
-    scanf("%f", &firstNum);
-    float secondNum;
-    printf("\nEnter the second number: ");
-    scanf("%f", &secondNum);
+    float firstNum, secondNum;
+    floatInput(&firstNum, &secondNum);
     float answer = firstNum  * secondNum;
     if((int)answer == answer) printf("The product of the first and second number is: %d", (int)answer);
     else printf("The product of the first and second number is: %.2f", firstNum * secondNum);
 }
 void div(){
-    float firstNum;
-    printf("Enter the first number: ");
-    scanf("%f", &firstNum);
-    float secondNum;
-    printf("\nEnter the second number: ");
-    scanf("%f", &secondNum);
+    float firstNum, secondNum;
+    floatInput(&firstNum, &secondNum);
     float answer = firstNum / secondNum;
     if((int)answer == answer) printf("The fraction of the provided numbers is: %d", (int)answer);
     else printf("The fraction of the provided numbers is: %.2f", answer);
