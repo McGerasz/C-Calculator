a0ae21e4b860cb1901ed917e266a522cf7960d32
McGerasz <mcgerasz@gmail.com>
2023-06-27 13:16:53 +0200

    added basic features

---
+++ Calculator.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+void sum(){
+    float firstNum;
+    printf("Enter the first number: ");
+    scanf("%f", &firstNum);
+    float secondNum;
+    printf("\nEnter the second number: ");
+    scanf("%f", &secondNum);
+    printf("The sum of the provided numbers is: %.2f", firstNum + secondNum);
+}
+void subt(){
+    float firstNum;
+    printf("Enter the first number: ");
+    scanf("%f", &firstNum);
+    float secondNum;
+    printf("\nEnter the second number: ");
+    scanf("%f", &secondNum);
+    printf("The second number subtracted from the first number is: %.2f", firstNum - secondNum);
+}
+void mult(){
+    float firstNum;
+    printf("Enter the first number: ");
+    scanf("%f", &firstNum);
+    float secondNum;
+    printf("\nEnter the second number: ");
+    scanf("%f", &secondNum);
+    printf("The product of the first and second number is: %.2f", firstNum * secondNum);
+}
+void div(){
+    float firstNum;
+    printf("Enter the first number: ");
+    scanf("%f", &firstNum);
+    float secondNum;
+    printf("\nEnter the second number: ");
+    scanf("%f", &secondNum);
+    printf("The fraction of the provided numbers is: %.2f", firstNum / secondNum);
+}
+void inputHandler(){
+    printf("Enter the desired operation:\n");
+    char operation[4];
+    const char list[] = "The currently valid inputs are: sum|+, subt|-, mult|*, div|/\n";
+    scanf("%s", operation);
+    if(strstr(operation, "list")) {
+        printf("%s", list);
+        inputHandler();
+    }
+    else if(strstr(operation, "sum") || strstr(operation, "+")) sum();
+    else if(strstr(operation, "subt")|| strstr(operation, "-")) subt();
+    else if(strstr(operation, "mult")|| strstr(operation, "*")) mult();
+    else if(strstr(operation, "div") || strstr(operation, "/")) div();
+    else {
+        printf("Invalid input!\n");
+        inputHandler();
+    }
+
+}
+int main(){
+    printf("Welcome to My Calculator!\n");
+    printf("If you would like to see a list of options enter \"list\"\n");
+    inputHandler();
+    return 0;
+}
